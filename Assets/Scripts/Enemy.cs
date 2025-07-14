d96c9e913473c548e2d194dcaa063583f58e3461
Terezzz13 <artemtarasov673@gmail.com>
2025-07-14 17:50:09 +0300

    CreateMap
    1.Мы добавили Assets.
    2. Мы отредактировали карту.
    3. Мы украшали карту.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected float timer;
+    protected float cooldown;
+    protected Transform player;
+    protected float distance;
+    protected float attackDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void Attack()
+    {
+        
+    }
+}
